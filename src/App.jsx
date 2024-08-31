fe8263d61e955a7c8016fc14c01e6a074e17022a
Lucas Tejerina <lucastejerina18@hotmail.com>
2024-08-30 21:24:39 -0300

    se agregaron las cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "./components/Navbar/Navbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import { ProductsData } from "./data/productData";
+
 
 function App() {
     return (
         <ChakraProvider>
             <Navbar />
-            <ItemListContainer greeting= "Bienvenidos a la Fairy Store!"/>
+            <ItemListContainer greeting= "Bienvenidos a la Fairy Store!" products= {ProductsData} />
         </ChakraProvider>
     );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,25 @@
 import { Box } from "@chakra-ui/react";
+import "./../ItemListContainer/ItemListContainer.css";
 
-const ItemListContainer = ({greeting}) => {
+const ItemListContainer = ({greeting, products}) => {
+    console.log (products);
+
     return (
-        <Box textAlign={"center"} fontSize={"40px"} fontFamily={"cursive"} > {greeting} </Box>
+        <Box className= "itemlist"> {
+            products.map((product) => {
+                return (
+                    <Box className="card">
+                    <img src={product.image} alt={product.name}  className="card__img"/>
+                    <Box className="card__container__info">
+                    <h2 className="card__title">{product.name}</h2>
+                    <p className="card__text">{product.description}</p>
+                    <p className="card__price">${product.price}</p>
+                    <button className="card__button">Comprar</button>
+                    </Box>
+                    </Box>
+                )
+            })
+        } </Box>
     )
 }
 
